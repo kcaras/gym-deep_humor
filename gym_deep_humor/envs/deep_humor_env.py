e8ff59202c4768962a58ad28d90172c39175b1fe
Kimberly Caras <kcaras6@gatech.edu>
2019-03-22 14:54:32 -0400

    removed main

--- gym_deep_humor/envs/deep_humor_env.py
+++ gym_deep_humor/envs/deep_humor_env.py
@@ -19,14 +19,3 @@ class DeepHumorEnv(gym.Env):
   def close(self):
     pass
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train a reinforcement learner.')
-
-    parser.add_argument('-f', '--model-file', required=True, help='The name of the model to load and save.')
-    parser.add_argument('-t', '--rl-test', help='Test the current model.', action='store_true')
-    parser.add_argument('-d', '--debug-print', help="Display more detail about the chatbot's decisions.", action='store_true')
-    parser.add_argument('-i', '--interactive', help='Directly talk to the chatbot instead of watching it talk to another chatbot.', action='store_true')
-    parser.add_argument('-o', '--show-other', help="Whether to show the unchosen chatbot's would-be response.", action='store_true')
-    parser.add_argument('-s', '--reduce-swear', help="Whether to reduce the amount of swearing used by the humor chatbot.", action='store_true')
-    args, _ = parser.parse_known_args()
